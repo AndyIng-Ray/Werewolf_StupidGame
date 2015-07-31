56b9cc722224e7c5449d28d7f451495aadcda4be
AndyIng <393117930@qq.com>
2015-07-31 21:17:53 +0800

    Add HunterShape & VillagerShape
    One werewolf kills several hunters and villages

--- app/src/main/java/classexample/only4hoursex4/Werewolf/Model.java
+++ app/src/main/java/classexample/only4hoursex4/Werewolf/Model.java
@@ -1,7 +1,14 @@
 package classexample.only4hoursex4.Werewolf;
 
+import android.graphics.Rect;
+
 /**
  * Created by andying on 7/31/15.
  */
 public class Model {
+    final int kShapeSize = 100;
+    final int kDefaultWorldSize = 200;
+
+    Rect mWorldBound;
+
 }
